369e968151bd12e14089a9b45fdfba01a780f8b9
gegyhamdani <gegyhamdani@gmail.com>
2020-04-04 23:16:47 +0700

    create db connection with pg

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const pool = require("./db");
 
-//middleware
+//Middleware
 app.use(cors());
 app.use(express.json());
+
+//Routes//
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
