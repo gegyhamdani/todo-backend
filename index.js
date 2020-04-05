22104015f12e566dbddf99937f50d610d9bcf465
gegyhamdani <gegyhamdani@gmail.com>
2020-04-05 20:28:41 +0700

    move all function to handlers folder

--- index.js
+++ index.js
@@ -8,75 +8,19 @@ app.use(cors());
 app.use(express.json());
 
 //Routes//
+const {
+  createTodo,
+  getAllTodo,
+  getTodo,
+  updateTodo,
+  deleteTodo,
+} = require("./handlers/todo");
 
-//Create a Todo
-app.post("/todos", async (req, res) => {
-  try {
-    const { description } = req.body;
-    const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES($1) RETURNING *",
-      [description]
-    );
-
-    res.json(newTodo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//Get all Todos
-app.get("/todos", async (req, res) => {
-  try {
-    const allTodos = await pool.query("SELECT * FROM todo");
-    res.json(allTodos.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//Get a Todo
-app.get("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
-      id,
-    ]);
-
-    res.json(todo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//Update a Todo
-app.put("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { description } = req.body;
-    const updateTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2",
-      [description, id]
-    );
-
-    res.json("Todo Was Updated");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//Delete a Todo
-app.delete("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
-      id,
-    ]);
-
-    res.json("Todo Was Deleted");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+app.post("/todos", createTodo);
+app.get("/todos", getAllTodo);
+app.get("/todos/:id", getTodo);
+app.put("/todos/:id", updateTodo);
+app.delete("/todos/:id", deleteTodo);
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
