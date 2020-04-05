e16caae523e446daf4ee62c4c1fb9590d58b73c7
gegyhamdani <gegyhamdani@gmail.com>
2020-04-05 19:18:32 +0700

    add get data routes

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ app.post("/todos", async (req, res) => {
   }
 });
 
+//Get all Todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.error(err.message);
+  }
+});
+
+//Get a Todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
 });
